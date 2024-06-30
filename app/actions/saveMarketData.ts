0fc3950b6222d3433a462adc5f9f4242a0a8f181
ilya smagin <ilya-z1999@ya.ru>
2024-06-30 16:24:22 +0300

    [Fix] Db save

--- app/actions/saveMarketData.ts
+++ app/actions/saveMarketData.ts
@@ -6,14 +6,19 @@ async function saveMarketData() {
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
 		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 	);
+	const dataPointAPI = await fetch(
+		"https://karelia.tele2.ru/api/exchange/lots/stats/volumes?trafficType=data"
+	);
+	const parsedDataPointAPI = await dataPointAPI.json();
+	const { meta, data } = await parsedDataPointAPI;
 	try {
 		const { error: dbError } = await supabase
 			.from("MarketData")
 			.insert([
 				{
-					status: "OK",
-					message: "testData",
-					data: `[{ "volume": 1.0, "minCost": 15.0, "avgCost": 16.49, "maxCost": 50.0, "count": 266165 }, { "volume": 2.0, "minCost": 30.0, "avgCost": 32.52, "maxCost": 100.0, "count": 110822 }, { "volume": 3.0, "minCost": 45.0, "avgCost": 47.58, "maxCost": 150.0, "count": 90680 }, { "volume": 4.0, "minCost": 60.0, "avgCost": 65.88, "maxCost": 200.0, "count": 36515 }, { "volume": 5.0, "minCost": 75.0, "avgCost": 81.75, "maxCost": 250.0, "count": 77991 }, { "volume": 6.0, "minCost": 90.0, "avgCost": 96.29, "maxCost": 300.0, "count": 30623 }}]`,
+					status: meta.status,
+					message: meta.message,
+					data: data,
 				},
 			])
 			.select();
@@ -23,7 +28,7 @@ async function saveMarketData() {
 	} catch (dbError) {
 		console.log(dbError);
 	} finally {
-		console.log("ended server action");
+		console.log("Server action: saveMarketData, status: OK");
 	}
 }
 export default saveMarketData;
