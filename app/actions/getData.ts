25aa697fac5cfec3a8d18dcd0dda38b30b5db0e2
lalacode <ilya-z1999@ya.ru>
2024-07-13 11:35:08 +0300

    Changed query to only get 1Gb count value instead of whole JSON

--- app/actions/getData.ts
+++ app/actions/getData.ts
@@ -9,7 +9,7 @@ async function getData(data_length: number) {
 
 	const { data, error } = await supabase
 		.from("MarketData")
-		.select("created_at, data")
+		.select("created_at, 1Gb")
 		.order("created_at", { ascending: false })
 		.limit(data_length);//supabase has internal nimit of 1000 TODO set it to week range (2014)
 	if (error) {
@@ -18,14 +18,14 @@ async function getData(data_length: number) {
 	//TODO write dedicated type
 	const newData =
 		(data as {
-			created_at: string;
-			data: { volume: number; count: number }[];
+			"created_at": string;
+			"1Gb": number;
 		}[]) || {};
 	const graphData = newData
 		.map((dataPoint) => {
 			return {
 				date: dataPoint.created_at,
-				numberOfLots: dataPoint.data[0].count,
+				numberOfLots: dataPoint["1Gb"],
 			};
 		})
 		.reverse();
