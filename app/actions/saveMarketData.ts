055629aedb6d51bd4b48f0616243b13d3149b22a
Ilya Smagin <ilya-z1999@ya.ru>
2026-06-22 00:28:01 +0300

    updated headers to access t2 api again

--- app/actions/saveMarketData.ts
+++ app/actions/saveMarketData.ts
@@ -7,10 +7,18 @@ async function saveMarketData() {
 		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 	);
 	const dataPointAPI = await fetch(
-		"https://karelia.tele2.ru/api/exchange/lots/stats/volumes?trafficType=data"
+		"https://api.t2.ru/api/exchange/lots/stats/volumes?trafficType=data",
+		{
+			headers: {
+				Accept: "application/json, text/plain, */*",
+				"User-Agent": "okhttp/4.2.0",
+				"Tele2-User-Agent":
+					'"mytele2-app/4.14.0"; "unknown"; "Android/11"; "Build/164755374"',
+				"X-API-Version": "1",
+			},
+		},
 	);
-	const parsedDataPointAPI = await dataPointAPI.json();
-	const { meta, data } = await parsedDataPointAPI;
+	const { meta, data } = await dataPointAPI.json();
 	const dbObject = {
 		status: meta.status,
 		message: meta.message,
