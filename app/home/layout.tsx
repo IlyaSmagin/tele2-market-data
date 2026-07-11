dc5fd2a86acd73daf9c8abf9aef2064f59938068
Ilya Smagin <ilya-z1999@ya.ru>
2026-07-11 15:35:00 +0300

    removed duplicate main tag

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -6,14 +6,10 @@ export default function Home({ children }: {
   return (
     <>
       <PageHeader title="Home" />
-      <main className="flex min-h-screen w-full flex-col bg-muted/40">
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-          <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-          </header>
-          <main className="grid flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 bg-muted/40">
+      <main className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+          <div className="grid flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 bg-muted/40">
             {children}
-          </main>
-        </div>
+          </div>
       </main>
     </>)
 }
